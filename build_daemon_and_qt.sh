e74fef8b88068239516d9fed1832569a7b5c72a4
Niels Cassens <siriusnlz@gmail.com>
2018-02-07 03:26:47 +0100

    new deps
    fixed some deps

--- build_daemon_and_qt.sh
+++ build_daemon_and_qt.sh
@@ -1,46 +1,19 @@
 echo Building daemon and qt...
 echo $COINFOLDER
-cd $COINFOLDER
 
-#./autogen.sh
-#if [ ${?} -ne 0 ]; then echo "daemon autogen failed."; read -n 1 -s; exit 1;fi
 
-LIBSH=$EWBPATHMSYS/$LIB
 
-CPPFLAGS="-I$LIBSH/$OPENSSL/include \
--I$LIBSH/$BERKELEYDB/build_unix \
--I$LIBSH/protobuf-$PROTOBUF/src \
--I$LIBSH/$MINIUPNPC \
--I$LIBSH/$LIBPNG \
--I$LIBSH/libevent-$LIBEVENT-stable/include \
-$ADDITIONALCCFLAGS"
-export CPPFLAGS
+cd $COINFOLDER/src/leveldb
+TARGET_OS=NATIVE_WINDOWS make libleveldb.a libmemenv.a
+if [ ${?} -ne 0 ]; then echo "leveldb failed."; read -n 1 -s; exit 1;fi
 
-# -I$LIBSH \
 
-LDFLAGS="-L$LIBSH/$OPENSSL \
--L$LIBSH/$BERKELEYDB/build_unix \
--L$LIBSH/$MINIUPNPC/miniupnpc \
--L$LIBSH/$LIBPNG/.libs \
--L$LIBSH/protobuf-$PROTOBUF/src/.libs \
--L$LIBSH/libevent-$LIBEVENT-stable/.libs"
-export LDFLAGS
 
-BOOST_ROOT=$LIBSH/$BOOST
-export BOOST_ROOT
 
-./configure \
---disable-upnp-default \
---disable-tests \
---with-qt-incdir=$QTPATHMSYS/include \
---with-qt-libdir=$QTPATHMSYS/lib \
---with-qt-plugindir=$QTPATHMSYS/plugins \
---with-qt-bindir=$QTPATHMSYS/bin \
---with-protoc-bindir=$LIBSH/protobuf-$PROTOBUF/src \
---with-gui=qt5
-#--with-miniupnpc=no
-# --disable-hardening
-# --enable-debug
+cp exp_buzzcoin.pro $COINFOLDER/buzzcoin.pro
+cp exp_makefile.mingw $COINFOLDER/src/makefile.mingw
+
+
 
 if [ ${?} -ne 0 ]; then echo "client configure failed."; read -n 1 -s; exit 1;fi
 
